754ecb4b04b7eb96d1d1bb05fe144df6a2fd50ff
waiyat.hamdani <waiyat.hamdani@fdmgroup.com>
2024-08-04 14:23:47 -0400

    web design for veteran clubs

---
+++ NccVetClubWebsite/javascript/script.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
