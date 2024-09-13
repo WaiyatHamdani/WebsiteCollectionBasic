443a30176335c404dcd507c29c03a2d6f6ab8ce5
waiyat.hamdani <waiyat.hamdani@fdmgroup.com>
2024-09-13 13:13:00 -0400

    waiyat schedule

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 Just a web design basic from my college text book and the one I thought a coole challange to design
 this just a quick example
 
+Waiyat Schedule
+![waiyatschedule](img/clock_demo.gif)
+
+
 
 bored and designed example
 ![design1](img/2.jpg)
---
+++ clock/rotation.js
@@ -0,0 +1,21 @@
+(function() {
+    function updateClock() {
+        var now = new Date();
+        
+        // Calculate degrees
+        var hourDeg = (now.getHours() % 12) / 12 * 360 + now.getMinutes() / 60 * 30;
+        var minuteDeg = now.getMinutes() / 60 * 360 + now.getSeconds() / 60 * 6;
+        var secondDeg = now.getSeconds() / 60 * 360;
+
+        // Update styles dynamically for each clock hand
+        document.querySelector('.clockhour').style.transform = 'rotate(' + hourDeg + 'deg)';
+        document.querySelector('.clockminute').style.transform = 'rotate(' + minuteDeg + 'deg)';
+        document.querySelector('.clocksecond').style.transform = 'rotate(' + secondDeg + 'deg)';
+    }
+
+    // Call updateClock every second
+    setInterval(updateClock, 1000);
+
+    // Initialize the clock immediately
+    updateClock();
+})();
